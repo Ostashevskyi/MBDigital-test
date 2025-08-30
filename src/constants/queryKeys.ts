897dd0f0559dc0345d111af07f620d1787c6a34d
Yurii Ostashevskyi <xpeke2015@gmail.com>
2025-08-30 19:07:52 +0300

    fix: remove everything from tanstack query

--- src/constants/queryKeys.ts
+++
@@ -1,1 +0,0 @@
-export const COURSES_GET_KEY = "courses_get"
--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,15 @@ import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { store } from "./redux/store.ts";
 
 import { router } from "./routes/router.tsx";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
